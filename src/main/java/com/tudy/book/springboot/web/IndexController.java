c575df35bba1ff5b0201491572c611ab946b2578
tudy <jhh0207@hanmail.net>
2021-08-03 19:01:03 +0900

    # 스프링시큐리티와 OAuth 2.0 로그인 기능 구현

--- src/main/java/com/tudy/book/springboot/web/IndexController.java
+++ src/main/java/com/tudy/book/springboot/web/IndexController.java
@@ -1,23 +1,35 @@
 package com.tudy.book.springboot.web;
 
+import com.tudy.book.springboot.config.auth.dto.SessionUser;
+import com.tudy.book.springboot.domain.user.User;
 import com.tudy.book.springboot.service.posts.PostsService;
 import com.tudy.book.springboot.web.dto.PostsResponseDto;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
