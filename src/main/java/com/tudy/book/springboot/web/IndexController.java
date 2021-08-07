f5e2bc5217be3ca4bd30752c4d5710a4ed479ff5
tudy <jhh0207@hanmail.net>
2021-08-07 23:58:26 +0900

    # 스프링시큐리티와 OAuth 2.0 로그인 기능을 어노테이션 기반으로 개선하기

--- src/main/java/com/tudy/book/springboot/web/IndexController.java
+++ src/main/java/com/tudy/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.tudy.book.springboot.web;
 
+import com.tudy.book.springboot.config.auth.LoginUser;
 import com.tudy.book.springboot.config.auth.dto.SessionUser;
 import com.tudy.book.springboot.domain.user.User;
 import com.tudy.book.springboot.service.posts.PostsService;
@@ -18,13 +19,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null){
             model.addAttribute("userName", user.getName());
